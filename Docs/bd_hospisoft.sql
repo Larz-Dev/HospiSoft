1f494b8b20a5a620c67d66c9d7faecf30b18434f
Larz <larzuck41@gmail.com>
2024-03-22 11:34:46 -0500

    Avance

--- Docs/bd_hospisoft.sql
+++ Docs/bd_hospisoft.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-03-2024 a las 14:12:16
+-- Tiempo de generación: 22-03-2024 a las 17:33:30
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -29,18 +29,25 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `hospisoft_detalleformula` (
   `Iddetalle` int(11) NOT NULL,
-  `consecutivoformula` int(11) NOT NULL,
   `iditem` int(11) NOT NULL,
   `cantidad` int(1) NOT NULL,
-  `posologia` varchar(250) NOT NULL
+  `posologia` varchar(250) NOT NULL,
+  `formula` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `hospisoft_detalleformula`
 --
 
-INSERT INTO `hospisoft_detalleformula` (`Iddetalle`, `consecutivoformula`, `iditem`, `cantidad`, `posologia`) VALUES
-(1, 1, 2, 20, 'asdqwdwdqwd');
+INSERT INTO `hospisoft_detalleformula` (`Iddetalle`, `iditem`, `cantidad`, `posologia`, `formula`) VALUES
+(1, 2, 20, 'asdqwdwdqwd', 1),
+(5, 1, 1, 'ggssg', 10),
+(6, 2, 2, 'adgsfh', 11),
+(7, 2, 2, 'adgsfh', 12),
+(8, 2, 2, 'adgsfh', 13),
+(9, 2, 2, 'adgsfh', 14),
+(10, 2, 2, 'adgsfh', 15),
+(11, 1, 677, 'fghdfhfgh', 16);
 
 -- --------------------------------------------------------
 
@@ -50,7 +57,6 @@ INSERT INTO `hospisoft_detalleformula` (`Iddetalle`, `consecutivoformula`, `idit
 
 CREATE TABLE `hospisoft_formula` (
   `consecutivo` int(11) NOT NULL,
-  `consecutivoformula` int(11) NOT NULL,
   `idMedico` int(11) NOT NULL,
   `idPaciente` int(11) NOT NULL,
   `fecha` date NOT NULL
@@ -60,8 +66,20 @@ CREATE TABLE `hospisoft_formula` (
 -- Volcado de datos para la tabla `hospisoft_formula`
 --
 
-INSERT INTO `hospisoft_formula` (`consecutivo`, `consecutivoformula`, `idMedico`, `idPaciente`, `fecha`) VALUES
-(1, 1, 1, 1, '2024-03-11');
+INSERT INTO `hospisoft_formula` (`consecutivo`, `idMedico`, `idPaciente`, `fecha`) VALUES
+(1, 1, 1, '2024-03-11'),
+(5, 1, 1, '2000-01-01'),
+(6, 1, 0, '2000-01-01'),
+(7, 1, 1, '2000-01-01'),
+(8, 1, 1, '2000-01-01'),
+(9, 1, 1, '2000-01-01'),
+(10, 1, 1, '2000-01-01'),
+(11, 1, 1, '2000-01-01'),
+(12, 1, 1, '2000-01-01'),
+(13, 1, 1, '2000-01-01'),
+(14, 1, 1, '2000-01-01'),
+(15, 1, 1, '2000-01-01'),
+(16, 2, 8, '2024-02-28');
 
 -- --------------------------------------------------------
 
@@ -80,8 +98,10 @@ CREATE TABLE `hospisoft_item` (
 --
 
 INSERT INTO `hospisoft_item` (`Iditem`, `descripcion`, `existencia`) VALUES
-(1, 'Píldoras', 100),
-(2, 'Bendaje', 20);
+(1, 'Píldorasssss', 100),
+(2, 'Bendaje', 20),
+(3, 'agua', 123),
+(4, 'ddhbeg', 323);
 
 -- --------------------------------------------------------
 
@@ -94,8 +114,18 @@ CREATE TABLE `hospisoft_medico` (
   `nombreMedico` varchar(200) NOT NULL,
   `apellidosMedico` varchar(250) NOT NULL,
   `emailMedico` varchar(250) NOT NULL,
-  `especialidadMedico` varchar(150) NOT NULL
+  `especialidadMedico` varchar(150) NOT NULL,
+  `rolMedico` varchar(10) NOT NULL,
+  `passwordMedico` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `hospisoft_medico`
+--
+
+INSERT INTO `hospisoft_medico` (`IdMedico`, `nombreMedico`, `apellidosMedico`, `emailMedico`, `especialidadMedico`, `rolMedico`, `passwordMedico`) VALUES
+(1, 'fdsf', 'safaws', 'a@gmail.com', 'Encargado', 'ROOT', '$2b$10$Cbc0nYiJNMKZrwei1vn0QOzsx2Oiymw4gEF31Vnc3ohMYWTB4v.XG'),
+(2, 'asds', 'asfaf', 'q@gmail.com', 'Encargado', 'Planta', '$2b$10$8YThXuzJwDAlA.xkDyT/QOCHlz4GA/Z3lrYjv66zmQoEarPWkmmWO');
 
 -- --------------------------------------------------------
 
@@ -115,6 +145,20 @@ CREATE TABLE `hospisoft_paciente` (
   `usuarioPaciente` varchar(250) NOT NULL,
   `passwordPaciente` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `hospisoft_paciente`
+--
+
+INSERT INTO `hospisoft_paciente` (`idPaciente`, `nombrePaciente`, `apellidosPaciente`, `emailPaciente`, `telefonoPaciente`, `movilPaciente`, `fechaNacimiento`, `epsPaciente`, `usuarioPaciente`, `passwordPaciente`) VALUES
+(1, 'Alejos', 'Tobon', 'asdas@gmail.com', '423423', '134134', '2005-03-13', 'asd', 'hola', '$2b$10$nQ9hcD5J5eXEYf6ZCD4aieVhvWAWRWsbCnrD8quZL/JhSkN1pH8di'),
+(7, 'Alguien', 'Mas', 'mundo@gmail.com', '123', '123', '2005-01-01', 'SOS', 'Objeto', '$2b$10$/WfG0YK5olZPAx4Ehv7TZ.eJt3CppgYXnhHsrmTbXy2QO/RlnZ8R2'),
+(8, 'Alguien', 'Mas', 'mundo@gmail.com', '123', '123', '2005-01-01', 'SOS', 'Objeto', '$2b$10$UQW/fvfS7GKKU0udG8./xudpGfaPcro6kHEakXIhWHBxRDVdOiGua'),
+(9, 'asdas', 'asdas', 'hola@gmail.com', '', '12313', '2024-02-28', 'fsdf', 'asd', '$2b$10$4IXgrS4X5bZrbK8I4Ixxj.JwM.r36UYX9uHmccgvQtliCbcayFiOe'),
+(10, 'fdsf', 'asf', 'hola@gmail.com', '4564', '312', '2024-03-12', 'dwd', 'asd', '$2b$10$WwZLXSbFR9LbkUiZE2Ahb.CqPgBo.Tly70dA7Kb1chY7Egstza/ci'),
+(11, 'dfsf', 'herth', 'asdasd@gmail.com', '', '1231', '2024-03-13', 'gfgre', 'fwrg', '$2b$10$cW4bMvNLjFp/4cuQHKs9Me50ET9gKEm1jVM3bTeW2Owa7oMnBrkQy'),
+(12, 'dfsf', 'herth', 'asdadsd@gmail.com', '', '1231', '2024-03-13', 'gfgre', 'fswrg', '$2b$10$V9nNqsRJ8Xdi/uIx4eJbu.PmfTpOPC1p6SVFKRKVBWdVdC8jFM9FS'),
+(13, 'gdfgdfhdh', 'faefwe', 'asdasd@gmail.comasdasd', '2132', '12314', '2024-03-31', 'dasd', 'dasd', '$2b$10$1ggQWfOVinZ6H4pP246RGOPXrluFBhbjx43Rz7zjfROqeOXBd2l5W');
 
 -- --------------------------------------------------------
 
@@ -144,8 +188,7 @@ ALTER TABLE `hospisoft_detalleformula`
 -- Indices de la tabla `hospisoft_formula`
 --
 ALTER TABLE `hospisoft_formula`
-  ADD PRIMARY KEY (`consecutivo`),
-  ADD KEY `hospisoft_formula_ibfk_1` (`consecutivoformula`);
+  ADD PRIMARY KEY (`consecutivo`);
 
 --
 -- Indices de la tabla `hospisoft_item`
@@ -179,31 +222,31 @@ ALTER TABLE `hospisoft_users`
 -- AUTO_INCREMENT de la tabla `hospisoft_detalleformula`
 --
 ALTER TABLE `hospisoft_detalleformula`
-  MODIFY `Iddetalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Iddetalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `hospisoft_formula`
 --
 ALTER TABLE `hospisoft_formula`
-  MODIFY `consecutivo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `consecutivo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de la tabla `hospisoft_item`
 --
 ALTER TABLE `hospisoft_item`
-  MODIFY `Iditem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Iditem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `hospisoft_medico`
 --
 ALTER TABLE `hospisoft_medico`
-  MODIFY `IdMedico` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `IdMedico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `hospisoft_paciente`
 --
 ALTER TABLE `hospisoft_paciente`
-  MODIFY `idPaciente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idPaciente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `hospisoft_users`
@@ -219,13 +262,8 @@ ALTER TABLE `hospisoft_users`
 -- Filtros para la tabla `hospisoft_detalleformula`
 --
 ALTER TABLE `hospisoft_detalleformula`
-  ADD CONSTRAINT `hospisoft_detalleformula_ibfk_1` FOREIGN KEY (`iditem`) REFERENCES `hospisoft_item` (`Iditem`);
-
---
--- Filtros para la tabla `hospisoft_formula`
---
-ALTER TABLE `hospisoft_formula`
-  ADD CONSTRAINT `hospisoft_formula_ibfk_1` FOREIGN KEY (`consecutivoformula`) REFERENCES `hospisoft_detalleformula` (`Iddetalle`);
+  ADD CONSTRAINT `hospisoft_detalleformula_ibfk_1` FOREIGN KEY (`iditem`) REFERENCES `hospisoft_item` (`Iditem`),
+  ADD CONSTRAINT `hospisoft_detalleformula_ibfk_2` FOREIGN KEY (`Iddetalle`) REFERENCES `hospisoft_formula` (`consecutivo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
